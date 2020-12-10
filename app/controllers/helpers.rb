e80f2b61abc9088293b043c33507eed6a83e3c22
nrymarz <nrymarz@gmail.com>
2020-12-09 21:43:57 -0600

    added form to create new recipes

--- app/controllers/helpers.rb
+++ app/controllers/helpers.rb
@@ -6,6 +6,24 @@ class Helpers
     def self.current_user(session)
         User.find(session[:user_id])
     end
+
+    def self.seperate_recipe_details(string)
+        arr = string.split("1.")
+        n = 2
+        if arr.last 
+            while(arr.last.include?("#{n}."))
+                substring = arr.last.split("#{n}.")
+                arr.pop
+                arr.push(substring)
+                n += 1
+                arr.flatten!
+            end
+            arr = arr[1..].collect{|s|s.strip}
+            arr.join('---')
+        else
+            ''
+        end
+    end
 end
       
     
--- app/controllers/recipes_controler.rb
+++ app/controllers/recipes_controler.rb
@@ -13,7 +13,24 @@ class RecipesController < ApplicationController
         erb :'recipes/edit'
     end
 
-    post '/recipes' do
+    post '/recipes/new' do
+        if Helpers.logged_in?(session)
+            params[:recipe][:ingredients] = Helpers.seperate_recipe_details(params[:recipe][:ingredients])
+            params[:recipe][:directions] = Helpers.seperate_recipe_details(params[:recipe][:directions])
+            if params[:recipe][:name].size > 0 && params[:recipe][:directions].size > 0
+                recipe = Recipe.create(params[:recipe])
+                recipe.chef = Helpers.current_user(session).name
+                recipe.user = Helpers.current_user(session)
+                recipe.save
+                redirect "/recipes/#{recipe.slug}"
+            else
+                @error = "Please Provide Recipe with Title and Directions"
+                erb :'/recipes/new'
+            end
+        else
+            @error = "You must be logged in to create recipes"
+            erb :'/recipes/new'
+        end
     end
 
     patch '/recipes/:slug' do 
